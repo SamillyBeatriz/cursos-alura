c286ce58561d22f5ea46d3f5f9a2108aa8c25646
Samilly Martins <samilly.beatriz23@gmail.com>
2024-11-01 16:51:45 -0300

    refatora lógica de troca do 'data-content' e 'src' para remover repetição de componentes e altera cores do título principal

--- GameMySQL/script.js
+++ GameMySQL/script.js
@@ -4,20 +4,48 @@ const rankingButton = document.querySelector('.ranking-button');
 const aboutButton = document.querySelector('.about-button')
 
 const gameTitle = document.querySelector('.app-title');
-const mysqlTitle = document.querySelector('.app-titleStrong');
+const strongTitle = document.querySelector('.app-titleStrong');
+const description = document.querySelector('.description')
+
+const bannerTitle = document.querySelector('.app-icons');
 
 startButton.addEventListener('click', ()=>{
-    html.setAttribute('data-contexto', 'first-backgroundColor');
-    mysqlTitle.style.color = '#ffff'
-    gameTitle.style.color = '#00618b'
-})
-rankingButton.addEventListener('click', ()=>{
-    html.setAttribute('data-contexto', 'second-backgroundColor');
-    mysqlTitle.style.color = '#00618b'
-    gameTitle.style.color = '#ffff'
-})
-aboutButton.addEventListener('click', ()=>{
-    html.setAttribute('data-contexto', 'third-backgroundColor');
-    mysqlTitle.style.color = '#ffff'
+
+    alterarEstilo('first-backgroundColor', 'mysql-icon');
+    strongTitle.style.color = '#00618b'
     gameTitle.style.color = '#e78e00'
 })
+rankingButton.addEventListener('click', ()=>{
+
+    alterarEstilo('second-backgroundColor', 'ranking-icon');
+    strongTitle.style.color = '#e78e00'
+    gameTitle.style.color = '#00618b'
+})
+aboutButton.addEventListener('click', ()=>{
+
+    alterarEstilo('third-backgroundColor', 'about-icon');
+    strongTitle.style.color = '#ffff'
+    gameTitle.style.color = '#e78e00'
+})
+
+function alterarEstilo(cor,caminho){
+    html.setAttribute('data-contexto', cor);
+    bannerTitle.setAttribute('src', `./images/${caminho}.png`);
+
+    switch (caminho) {
+        case "mysql-icon":
+            description.innerHTML = `
+            Start <strong class="app-titleStrong"> Playing</strong>`
+            break;
+        case "ranking-icon":
+            description.innerHTML = `
+            Player <strong class="app-titleStrong"> Rankings</strong>`
+            break;
+        case "about-icon":
+            description.innerHTML = `
+            About <strong class="app-titleStrong"> me </strong>`
+            break;
+        default:
+            break;
+    }
+}
