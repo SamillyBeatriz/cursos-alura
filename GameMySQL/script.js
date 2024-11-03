078da5649b48505c511a3c7cf048d899f105db63
Samilly Martins <samilly.beatriz23@gmail.com>
2024-11-02 23:31:33 -0300

    Configura estado visual dos botões com alternância da classe 'active'

--- GameMySQL/script.js
+++ GameMySQL/script.js
@@ -2,6 +2,7 @@ const html = document.querySelector('html');
 const startButton = document.querySelector('.start-button');
 const rankingButton = document.querySelector('.ranking-button');
 const aboutButton = document.querySelector('.about-button')
+const botoes = document.querySelectorAll('.btn-action')
 
 const gameTitle = document.querySelector('.app-title');
 const strongTitle = document.querySelector('.app-titleStrong');
@@ -14,21 +15,28 @@ startButton.addEventListener('click', ()=>{
     alterarEstilo('first-backgroundColor', 'mysql-icon');
     strongTitle.style.color = '#00618b'
     gameTitle.style.color = '#e78e00'
+    startButton.classList.add('active')
 })
 rankingButton.addEventListener('click', ()=>{
 
     alterarEstilo('second-backgroundColor', 'ranking-icon');
     strongTitle.style.color = '#e78e00'
     gameTitle.style.color = '#00618b'
+    rankingButton.classList.add('active')
 })
 aboutButton.addEventListener('click', ()=>{
 
     alterarEstilo('third-backgroundColor', 'about-icon');
     strongTitle.style.color = '#ffff'
     gameTitle.style.color = '#e78e00'
+    aboutButton.classList.add('active')
 })
 
 function alterarEstilo(cor,caminho){
+    botoes.forEach(function(caminho){
+            caminho.classList.remove('active')
+    })
+
     html.setAttribute('data-contexto', cor);
     bannerTitle.setAttribute('src', `./images/${caminho}.png`);
 
